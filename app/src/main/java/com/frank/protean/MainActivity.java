faab0aaf040f24b83fdea9851a6b7b6bb50e5e38
maowenqiang <Frank0701@163.com>
2016-07-30 01:45:50 +0800

    新增volley测试代码

--- app/src/main/java/com/frank/protean/MainActivity.java
+++ app/src/main/java/com/frank/protean/MainActivity.java
@@ -4,12 +4,43 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
+    private RequestQueue mRequestQueue;
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("MainActivity","first change!");
+        Log.d("MainActivity", "first change!");
+        mRequestQueue =  Volley.newRequestQueue(this);
+        loadData();
+    }
+
+    private void loadData() {
+        String url ="http://app.jyall.com/jypay-cashier/v1/queryBusinType";
+        JsonObjectRequest jr = new JsonObjectRequest(Request.Method.GET,url,null,new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d(TAG,response.toString());
+                Log.d(TAG,Thread.currentThread().getName());
+
+            }
+        },new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(TAG,error.getMessage());
+            }
+        });
+        mRequestQueue.add(jr);
     }
 }
